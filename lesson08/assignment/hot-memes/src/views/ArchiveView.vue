d419ec260a2884fdeee72cdf9e3302ee00420a9a
Jeremy <j1lin0101@gmail.com>
2022-03-05 17:04:30 -0800

    Reconfigure Archive and Hot Into Components

---
+++ lesson08/assignment/hot-memes/src/views/ArchiveView.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container py-4">
+    <h1>Legendary Memes from a Simpler Time</h1>
+    <img src="/assets/logo.png" alt="" />
+    <div class="container">
+      <div class="row">
+        <div class="col col-lg-12">
+          <h2>Memes Sorted By Year Popularized</h2>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-lg-12">
+          <h3>2010</h3>
+        </div>
+      </div>
+
+      <div
+        class="row g-2 g-lg-3"
+        v-for="meme in archiveMemes"
+        v-bind:meme="meme"
+        v-bind:key="meme.id"
+      >
+        <div class="col col-sm-12 col-md-6 col-lg-4">
+          <img
+            v-bind:src="meme.image"
+            class="d-block w-50 img-fluid mx-auto"
+            v-if="meme.show"
+          />
+        </div>
+
+        <div class="col col-sm-12 col-md-6 col-lg-8">
+          <h4>{{ meme.title }}</h4>
+          <p>
+            {{ meme.description }}
+          </p>
+          <p>
+            Source:
+            <a :href="meme.source">KnowYourMeme</a>
+          </p>
+          <p>
+            <button v-on:click="meme.show = !meme.show">Show the Meme!</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArchiveView",
+  data() {
+    return {
+      archiveMemes: [
+        {
+          id: 1,
+          show: true,
+          image: "../../images/2010/knee.jpeg",
+          title: "I Took an Arrow to the Knee",
+          description:
+            "'I Took an Arrow in the Knee' is a memorable quote originating from the role-playing video game The Elder Scrolls V: Skyrim. An often repeated stock line throughout the game, it went on to inspire the snowclone 'I used to X, then I took an arrow to the knee'.",
+          source: "https://knowyourmeme.com/memes/i-took-an-arrow-in-the-knee",
+        },
+        {
+          id: 2,
+          show: false,
+          image: "../../images/2010/morpheus.jpeg",
+          title: "Matrix Morpheus",
+          description:
+            "Matrix Morpheus is an advice animal image macro series featuring a screen capture of the fictional character Morpheus (played by Lawrence Fishburne) from The Matrix franchise. The captions typically follow a two line format with the first line reading 'What if I Told You,' followed by a revelation that often belittles futile or petty behavior, in a similar vein to the advice animal characters Conspiracy Keanu and Sudden Clarity Clarence.",
+          source: "https://knowyourmeme.com/memes/matrix-morpheus",
+        },
+        {
+          id: 3,
+          show: false,
+          image: "../../images/2010/orange.jpeg",
+          title: "Annoying Orange",
+          description:
+            "The Annoying Orange is an american comedy web series created by Dane Boedingheimer for his own company Gagfilms in 2009 starring an anthropomorphic orange annoying other fruits, vegetables and other objects that appear in the kitchen.",
+          source:
+            "https://knowyourmeme.com/memes/subcultures/the-annoying-orange",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ lesson08/assignment/hot-memes/src/views/HotView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container py-4">
+    <h1>Hottest Memes of the Week</h1>
+
+    <h3>Highlights</h3>
+
+    <h3>Other Hot Memes</h3>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <div
+        class="col"
+        v-for="image in images"
+        v-bind:image="image"
+        v-bind:key="image.id"
+      >
+        <div class="card shadow-sm">
+          <img v-bind:src="image.src" class="d-block w-50 img-fluid mx-auto" />
+
+          <div class="card-body">
+            <p class="card-text">
+              This is a place holder meme taken from (Source)
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a v-bind:href="image.src"
+                  ><button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button></a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HotView",
+  data() {
+    return {
+      images: [
+        {
+          id: 1,
+          src: "../../images/ceo.jpg",
+          description: "CEO Meme",
+        },
+        {
+          id: 2,
+          src: "../../images/2010/knee.jpeg",
+          description: "Arrow to the Knee",
+        },
+        {
+          id: 3,
+          src: "../../images/2010/morpheus.jpeg",
+          description: "Morpheus",
+        },
+      ],
+    };
+  },
+};
+</script>
